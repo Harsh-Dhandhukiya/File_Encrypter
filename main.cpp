70f7573c29605da648e128bf822898346e8a19f3
Harsh-Dhandhukiya <25harsh012005@gmail.com>
2025-08-28 14:14:50 +0530

    Testing DevOps (GitHub)

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// #include<bits/stdc++.h>
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int a = 5;
+    int b = 10;
+    int c = a+b;
+
+
+    cout<< c << endl;
+
+    return 0;
+}
